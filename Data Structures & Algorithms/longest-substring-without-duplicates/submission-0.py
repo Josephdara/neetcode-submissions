0e7367344a9417a6b38bd7735bffeb97d53e5d29
Joseph Dara Olutimehin <jsatimehin@gmail.com>
2026-09-23 11:19:48 -0400

    Add: longest-substring-without-duplicates - submission-0

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-0.py
@@ -0,0 +1,18 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        left = 0
+        right = 0
+        maxLength = 0
+        solution_set = set()
+        for right in range(len(s)):
+            while s[right] in solution_set:
+                solution_set.remove(s[left])
+                left += 1
+            solution_set.add(s[right])
+            if len(solution_set) > maxLength:
+                maxLength = len(solution_set) 
+        return maxLength
+
+
+
+        
